0d0450377f908dbffe0fb948f0b5671eff45fbb9
Mathieu <github@kumy.net>
2018-07-07 14:20:30 +0200

    move to top

--- app/initializers/reset-scroll-position.js
+++ app/initializers/reset-scroll-position.js
@@ -3,11 +3,11 @@ import Route from '@ember/routing/route';
 import ResetScrollPositionMixin from '../mixins/reset-scroll-position';
 
 export function initialize(/* application */) {
-  // Route.reopen(ResetScrollPositionMixin);
+  Route.reopen(ResetScrollPositionMixin);
   // application.inject('route', 'foo', 'service:foo');
 }
 
 export default {
-  // name: 'reset-scroll-position',
+  name: 'reset-scroll-position',
   initialize
 };

1213359bb8c054673a41bbd081013e9a81df46d9
Mathieu <github@kumy.net>
2018-07-09 22:26:21 +0200

    Use mirage in integration

--- tests/integration/components/news-comment-show-test.js
+++ tests/integration/components/news-comment-show-test.js
@@ -2,37 +2,24 @@ import {module, test} from 'qunit';
 import {setupRenderingTest} from 'ember-qunit';
 import {render} from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
-import EmberObject from '@ember/object';
-// import { startMirage } from 'geokrety-front/initializers/ember-cli-mirage';
+import {startMirage} from 'geokrety-front/initializers/ember-cli-mirage';
 
 module('Integration | Component | news comment show', function(hooks) {
   setupRenderingTest(hooks);
-  // beforeEach() {
-  //   this.server = startMirage();
-  // };
-  // afterEach() {
-  //   this.server.shutdown();
-  // };
-
-  const NewsComment = EmberObject.extend({
-    id: 5,
-    newsId: "181",
-    userId: "26422",
-    comment: "Congrats",
-    icon: "1",
-    "createdOnDateTime": "2018-04-17T20:30:02"
+  hooks.beforeEach(function() {
+    this.server = startMirage();
+  });
+  hooks.afterEach(function() {
+    this.server.shutdown();
   });
 
   test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
-    let comment = NewsComment.create();
+    let comment = server.create('newsComment');
     this.set('comment', comment);
 
     await render(hbs `{{news-comment-show comment=comment}}`);
 
-    assert.equal(this.element.querySelector('[data-test-news-comment-author]').textContent.trim(), 'Anonymous');
-    assert.equal(this.element.querySelector('[data-test-news-comment-content]').textContent.trim(), 'Congrats');
-    assert.equal(this.element.querySelector('[data-test-news-comment-creation-date-time]').textContent.trim(), 'April 17, 2018');
+    assert.equal(this.element.querySelector('[data-test-news-comment-author]').textContent.trim(), comment.author.name);
+    assert.equal(this.element.querySelector('[data-test-news-comment-content]').textContent.trim(), comment.comment);
   });
 });
